cf18148945f990035de9a6e26f72b5a8d7913acb
0xfff <0xfff@protonmail.com>
2016-09-10 00:36:50 +0200

    (Update) Build: Implement webpack with babel and es2016  + WebGL Globe

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: './src/js/dash-masternode-globe.js',
+  output: {
+    filename: 'dist/dash-masternode-globe.min.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+            presets: ['es2016']
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'exports'
+      },
+      {
+        test: /\.js$/,
+        loader: 'imports'
+      }
+    ]
+  }
+};
